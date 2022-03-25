eb1a9bd7d1b58bbcd8107dbbe2cb1664f0b58d16
yanliangli <z3200271@gmail.com>
2022-03-25 16:48:07 -0400

    Create solution class to parse input

---
+++ src/main/java/main/Parser.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Parser {
+
+    /**
+     * Given a strings of bracket characters: {}(), check if the string is balanced.
+     * @param s - a string of characters to check for balance. S consists only of the characters {, }, (, and ). S.length() < 50.
+     * @return a boolean that denotes whether the string is balanced : true if string is balanced, or false if it is not.
+     */
+    public boolean isBalanced(String s) {
+        return true;
+    }
+}
---
+++ src/main/java/main/Solution.java
@@ -0,0 +1,25 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Parser parser = new Parser();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter string inputs: ");
+        List<String> inputList = new ArrayList<>();
+        String nextInput = scanner.nextLine();
+        if (nextInput.isEmpty()) {
+            return;
+        }
+        while (!nextInput.isEmpty()) {
+            inputList.add(nextInput);
+            nextInput = scanner.nextLine();
+        }
+        inputList.stream().map(parser::isBalanced).forEach(System.out::println);
+    }
+}
+
