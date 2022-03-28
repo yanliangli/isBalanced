1b6160d453d908edde136512a3d9cc276d9232b3
yanliangli <z3200271@gmail.com>
2022-03-28 15:40:49 -0400

    Add error handling to input parsing

--- src/main/java/com/demo/Solution.java
+++ src/main/java/com/demo/Solution.java
@@ -1,30 +1,46 @@
 package com.demo;
 
+import com.demo.exception.IsBalancedParseException;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
+    private static final String VALID_INPUT_FORMAT = "[(,),{,}]{0,49}";
 
     public static void main(String[] args) {
         Parser parser = new Parser();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter string inputs: ");
-        String nextInput = scanner.nextLine();
-        if (nextInput.isEmpty()) {
-            return;
+        List<String> inputList = new ArrayList<>();
+        try {
+            inputList = parseInput(scanner);
+        } catch (IsBalancedParseException e) {
+            System.out.println("Failed to parse user inputs. Error = " + e.getMessage());
         }
-        List<String> inputList = parseInput(scanner, nextInput);
         displayOutput(parser, inputList);
     }
 
-    private static List<String> parseInput(Scanner scanner, String nextInput) {
+    private static List<String> parseInput(Scanner scanner) {
         List<String> inputList = new ArrayList<>();
+        String nextInput = scanner.nextLine();
+        if (nextInput.isEmpty()) {
+            throw new IsBalancedParseException("Invalid input");
+        }
         while (!nextInput.isEmpty()) {
+            if (!validateInput(nextInput)) {
+                throw new IsBalancedParseException("Invalid input. Please enter input only consist (, ), {, and }, " +
+                        "and ensure the total number of characters in each input string is less than 50.");
+            }
             inputList.add(nextInput);
             nextInput = scanner.nextLine();
         }
         return inputList;
+    }
+
+    private static boolean validateInput(String nextInput) {
+        return nextInput.matches(VALID_INPUT_FORMAT);
     }
 
     private static void displayOutput(Parser parser, List<String> inputList) {
---
+++ src/main/java/com/demo/exception/IsBalancedParseException.java
@@ -0,0 +1,9 @@
+package com.demo.exception;
+
+
+public class IsBalancedParseException extends RuntimeException{
+
+    public IsBalancedParseException (String message) {
+        super(message);
+    }
+}
